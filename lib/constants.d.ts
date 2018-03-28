fde9c0f400538fd978f11693926464b04d382985
Paul Smith <paul@elandesign.co.uk>
2018-03-28 12:44:22 +0100

    Build

---
+++ lib/constants.d.ts
@@ -0,0 +1,4 @@
+export declare const APOLLO_OVERWRITE = "APOLLO_OVERWRITE";
+export declare const APOLLO_RESET = "APOLLO_RESET";
+export declare const APOLLO_WRITE = "APOLLO_WRITE";
+export declare const APOLLO_DELETE = "APOLLO_DELETE";
